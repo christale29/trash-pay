e3cd5f9852e603c5223f9237e806b7360f019b44
Uwanziza <uwachanelle@gmail.com>
2022-02-07 16:21:07 +0200

    Footer

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Footer.css';
+
+const Footer=()=>{
+    return(
+<div class='Footer-container'>
+    
+<small>&copy; Copyright 2021, CEC</small>
+<div class ="Footer_words">
+<h1>TRASHPAY</h1>
+</div>    
+</div>
+
+
+
+    )
+}
+export default Footer;
